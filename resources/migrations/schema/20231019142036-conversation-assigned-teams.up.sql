874d30a4299eab7809b6b2c1570980c7fcdda6ee
ryan echternacht <ryanechternacht@ryans-MacBook-Pro-2.local>
2023-10-21 14:15:51 -0400

    add collaboration types and support these.
    also fix a bug where assigned to is null when updating a conversation (which is now legal)

--- resources/migrations/schema/20231019142036-conversation-assigned-teams.up.sql
+++ resources/migrations/schema/20231019142036-conversation-assigned-teams.up.sql
@@ -1,6 +1,7 @@
 alter table buyersphere_conversation alter column assigned_to drop not null;
 --;;
 
+
 alter table buyersphere_conversation add column assigned_team text;
 --;;
 update buyersphere_conversation 
@@ -11,3 +12,12 @@ where buyersphere_conversation.assigned_to = buyersphere_user_account.id;
 alter table buyersphere_conversation alter column assigned_team set not null;
 --;;
 alter table buyersphere_conversation add constraint buyersphere_assigned_team check (assigned_team in ('buyer', 'seller'));
+--;;
+
+
+alter table buyersphere_conversation add column collaboration_type text not null;
+--;;
+update buyersphere_conversation
+set collaboration_type = 'question';
+--;;
+alter table buyersphere_conversation add constraint buyersphere_conversation_collaboration_type check (collaboration_type in ('task', 'question', 'comment', 'meeting'));
--- src/partnorize_api/data/conversations.clj
+++ src/partnorize_api/data/conversations.clj
@@ -13,19 +13,19 @@
                 :buyersphere_conversation.resolved
                 :buyersphere_conversation.due_date
                 :buyersphere_conversation.created_at
-                [:user_account_author.id :author_id] 
-                [:user_account_author.first_name :author_first_name] 
+                [:user_account_author.id :author_id]
+                [:user_account_author.first_name :author_first_name]
                 [:user_account_author.last_name :author_last_name]
                 [:user_account_author.display_role :author_display_role]
-                [:user_account_assigned_to.id :assigned_to_id] 
-                [:user_account_assigned_to.first_name :assigned_to_first_name] 
+                [:user_account_assigned_to.id :assigned_to_id]
+                [:user_account_assigned_to.first_name :assigned_to_first_name]
                 [:user_account_assigned_to.last_name :assigned_to_last_name]
                 [:user_account_assigned_to.display_role :assigned_to_display_role])
       (h/from :buyersphere_conversation)
-      (h/join [:user_account :user_account_author] 
+      (h/join [:user_account :user_account_author]
               [:= :buyersphere_conversation.author :user_account_author.id])
-      (h/join [:user_account :user_account_assigned_to]
-              [:= :buyersphere_conversation.assigned_to :user_account_assigned_to.id])
+      (h/left-join [:user_account :user_account_assigned_to]
+                   [:= :buyersphere_conversation.assigned_to :user_account_assigned_to.id])
       (h/where [:= :buyersphere_conversation.organization_id organization-id]
                [:= :buyersphere_conversation.buyersphere_id buyersphere-id])
       (h/order-by :buyersphere_conversation.updated_at)))
@@ -41,16 +41,17 @@
                       :last_name author_last_name
                       :display_role author_display_role})))
 
-(defn- reformat-assigned-to [{:keys [assigned_to_id 
+(defn- reformat-assigned-to [{:keys [assigned_to_id
                                      assigned_to_first_name
                                      assigned_to_last_name
                                      assigned_to_display_role] :as conversation}]
   (-> conversation
       (dissoc :assigned_to_first_name :assigned_to_last_name :assigned_to_display_role)
-      (assoc :assigned_to {:id assigned_to_id
-                           :first_name assigned_to_first_name
-                           :last_name assigned_to_last_name
-                           :display_role assigned_to_display_role})))
+      (cond->
+       assigned_to_id (assoc :assigned_to {:id assigned_to_id
+                                           :first_name assigned_to_first_name
+                                           :last_name assigned_to_last_name
+                                           :display_role assigned_to_display_role}))))
 
 (defn get-by-buyersphere [db organization-id buyersphere-id]
   (->> (base-conversation-query organization-id buyersphere-id)
@@ -79,14 +80,15 @@
   (let [{:keys [first_name last_name display_role id]} (users/get-by-id db organization-id (:assigned_to conversation))]
     (-> conversation
         (dissoc :assigned_to)
-        (assoc :assigned_to {:id id
-                             :first_name first_name
-                             :last_name last_name
-                             :display_role display_role}))))
+        (cond->
+         id (assoc :assigned_to {:id id
+                                 :first_name first_name
+                                 :last_name last_name
+                                 :display_role display_role})))))
 
 (defn update-conversation [db organization-id buyersphere-id conversation-id body]
   (let [fields (cond-> (select-keys body [:resolved :message :due-date :assigned-to])
-                 :due-date (update :due-date inst/read-instant-date))
+                 (:due-date body) (update :due-date inst/read-instant-date))
         result (-> (h/update :buyersphere_conversation)
                    (h/set fields)
                    (h/where [:= :buyersphere_conversation.organization_id organization-id]
@@ -102,5 +104,6 @@
   (get-by-buyersphere db/local-db 1 1)
   (create-conversation db/local-db 1 1 1 "hello, world!" "2012-02-03" 5)
   (update-conversation db/local-db 1 1 29 {:message "goodbye! hello" :resolved false :due-date "2023-10-05T22:00:00.000Z" :assigned-to 2})
+  (update-conversation db/local-db 1 1 29 {:message "goodbye! hello" :resolved false :assigned-to 2})
   ;
   )
