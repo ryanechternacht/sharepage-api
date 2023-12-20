a72863ef65b9b57038d74c61ba186767a3e6b004
ryan echternacht <ryan@echternacht.org>
2023-12-19 22:51:47 -0500

    Add schema for buyer_tracking table

---
+++ resources/migrations/schema/20231220033540-user-tracking.down.sql
@@ -0,0 +1,1 @@
+drop table buyer_tracking;
---
+++ resources/migrations/schema/20231220033540-user-tracking.up.sql
@@ -0,0 +1,15 @@
+create table buyer_tracking (
+  organization_id int references organization(id),
+  buyersphere_id int references buyersphere(id),
+  user_account_id int references user_account(id),
+  activity text not null,
+  created_at timestamp with time zone not null,
+  constraint buyer_tracking_activity check (activity in ('login'))
+);
+--;;
+create trigger buyer_tracking_insert_timestamp
+before insert on buyer_tracking
+for each row execute procedure trigger_insert_timestamps();
+--;;
+create index idx_buyer_tracking_organization_id_buyersphere_id 
+on buyer_tracking(organization_id, buyersphere_id);
