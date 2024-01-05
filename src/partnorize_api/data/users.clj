bd464ba72844ceae8b10407345cf95014a827509
ryan echternacht <ryan@echternacht.org>
2024-01-04 23:04:46 -0500

    Add team to all users calls

--- src/partnorize_api/data/users.clj
+++ src/partnorize_api/data/users.clj
@@ -17,6 +17,17 @@
       (h/where [:= :user_account.organization_id organization-id])
       (h/order-by :user_account.first_name :user_account.last_name)))
 
+(def ^:private role-map {"admin" "seller"
+                         "buyer" "buyer"})
+
+(defn- add-team [{:keys [buyersphere_role] :as user}]
+  (-> user
+      (assoc :team (role-map buyersphere_role))))
+
+(defn format-user [user]
+  (-> user
+      add-team))
+
 ;; TODO I don't love how global admin stuff has to pollute this 
 ;; to work correctly
 (defn get-by-email [db organization-id email]
@@ -26,51 +37,61 @@
         global-admin-query (-> (apply h/select user-columns)
                                (h/from :user_account)
                                (h/where [:= :user_account.email email]
-                                        [:is :user_account.is_admin :true]))]
-    (-> (h/union user-in-org-query
-                 global-admin-query)
-        (h/order-by [:is_admin :desc])
-        (db/->execute db)
-        first)))
+                                        [:is :user_account.is_admin :true]))
+        full-query (-> (h/union user-in-org-query
+                                global-admin-query)
+                       (h/order-by [:is_admin :desc]))]
+    (->> full-query
+         (db/->>execute db)
+         (map format-user)
+         first)))
 
 (defn get-by-organization [db organization-id]
-  (-> (base-user-query organization-id)
-      (h/where [:= :user_account.buyersphere_role "admin"])
-      (db/->execute db)))
+  (let [query (-> (base-user-query organization-id)
+                  (h/where [:= :user_account.buyersphere_role "admin"]))]
+    (->> query
+         (db/->>execute db)
+         (map format-user))))
 
 (defn get-by-id [db organization-id id]
-  (-> (base-user-query organization-id)
-      (h/where [:= :user_account.id id])
-      (db/->execute db)
-      first))
+  (let [query (-> (base-user-query organization-id)
+                  (h/where [:= :user_account.id id]))]
+    (->> query
+         (db/->>execute db)
+         (map format-user)
+         first)))
 
 (defn get-by-email-global
   "Finds a user, not bounded by the current org. Returns all user
    accounts created for the user, sorted by db id for predictability.
    This is intended for the generalized login page."
   [db email]
-  (-> (apply h/select user-columns)
-      (h/from :user_account)
-      (h/where [:= :user_account.email email])
-      (h/order-by :user_account.id)
-      (db/->execute db)))
+  (let [query (-> (apply h/select user-columns)
+                  (h/from :user_account)
+                  (h/where [:= :user_account.email email])
+                  (h/order-by :user_account.id))]
+    (->> query
+         (db/->>execute db)
+         (map format-user))))
 
 (defn create-user [config db organization buyersphere-role {:keys [first-name last-name display-role email]}]
   (let [stytch-member-id (stytch/create-user (:stytch config)
                                              (:stytch-organization-id organization)
                                              email
-                                             (str first-name " " last-name))]
-    (stytch/send-magic-link-email (:stytch config)
-                                  (:stytch-organization-id organization)
-                                  email)
-    (-> (h/insert-into :user_account)
-        (h/columns :organization_id :buyersphere_role :first_name
-                   :last_name :display_role :email :stytch_member_id)
-        (h/values [[(:id organization) buyersphere-role first-name
-                    last-name display-role email stytch-member-id]])
-        (#(apply h/returning % user-columns))
-        (db/->execute db)
-        first)))
+                                             (str first-name " " last-name))
+        _ (stytch/send-magic-link-email (:stytch config)
+                                        (:stytch-organization-id organization)
+                                        email)
+        query (-> (h/insert-into :user_account)
+                  (h/columns :organization_id :buyersphere_role :first_name
+                             :last_name :display_role :email :stytch_member_id)
+                  (h/values [[(:id organization) buyersphere-role first-name
+                              last-name display-role email stytch-member-id]])
+                  (#(apply h/returning % user-columns)))]
+    (->> query
+         (db/->>execute db)
+         (map format-user)
+         first)))
 
 (defn update-user-from-stytch [db email name image]
   (try
@@ -92,15 +113,17 @@
       nil)))
 
 (defn update-user [db organization-id id {:keys [first-name last-name display-role email]}]
-  (-> (h/update :user_account)
-      (h/set {:first_name first-name
-              :last_name last-name
-              :display_role display-role})
-      (h/where [:= :organization_id organization-id]
-               [:= :id id])
-      (#(apply h/returning % user-columns))
-      (db/->execute db)
-      first))
+  (let [query (-> (h/update :user_account)
+                  (h/set {:first_name first-name
+                          :last_name last-name
+                          :display_role display-role})
+                  (h/where [:= :organization_id organization-id]
+                           [:= :id id])
+                  (#(apply h/returning % user-columns)))]
+    (->> query
+         (db/->>execute db)
+         (map format-user)
+         first)))
 
 (comment
   (get-by-email db/local-db 1 "ryan@echternacht.org")
@@ -116,7 +139,7 @@
                "buyer"
                {:first-name "grace"
                 :last-name "ooi"
-                :email "grace11@echternacht.org"
+                :email "grace12@echternacht.org"
                 :display-role "my love"})
   (update-user-from-stytch db/local-db "ryan@echternacht.org" "bill nye the third" "https://www.google.com")
   (update-user db/local-db 1 14 {:first-name "grace" :last-name "ooi"
