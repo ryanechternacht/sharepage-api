e5120a1ffe079f345d0f63e24dbcb4e568f2788c
ryan echternacht <ryan@echternacht.org>
2024-06-29 12:15:05 -0400

    Update up script to also rollout campaign row numbers corectly

--- resources/migrations/20240627212711-campaign-row-number.up.sql
+++ resources/migrations/20240627212711-campaign-row-number.up.sql
@@ -1,2 +1,16 @@
 alter table buyersphere
 add column campaign_row_number int;
+--;;
+update buyersphere
+set campaign_row_number = rownumber - 1
+from (
+    select
+        id, 
+        campaign_uuid, 
+        campaign_row_number, 
+        ROW_NUMBER() over (partition by campaign_uuid order by id) as rownumber
+    from
+        buyersphere
+    where campaign_uuid is not null
+) as in_campaign
+where buyersphere.id = in_campaign.id;
