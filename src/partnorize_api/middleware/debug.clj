e3e891159d6557978e420afc344336530bbd3eb2
ryan echternacht <ryan@echternacht.org>
2024-07-01 14:30:05 -0400

    Add in code to automatically enable logging in dev/prod

--- src/partnorize_api/middleware/debug.clj
+++ src/partnorize_api/middleware/debug.clj
@@ -1,9 +1,10 @@
 (ns partnorize-api.middleware.debug)
 
-(defn- wrap-debug-impl [handler {:keys [request-method uri] :as request}]
-  (println "request" request-method uri request)
-  (let [response (handler request)]
-    (println "response" request-method uri response)
-    response))
+(defn- wrap-debug-impl [handler {:keys [config request-method uri] :as request}]
+  (let [print? (-> config :debug :print?)]
+    (when print? (println "request" request-method uri request))
+    (let [response (handler request)]
+      (when print? (println "response" request-method uri response))
+      response)))
 
 (defn wrap-debug [h] (partial #'wrap-debug-impl h))
