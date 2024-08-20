6f7bec9ee978793ff59e849b2f82877a2b68dccc
ryan echternacht <ryan@echternacht.org>
2024-08-20 14:51:16 -0400

    committing some wip

--- src/partnorize_api/external_api/open_ai.clj
+++ src/partnorize_api/external_api/open_ai.clj
@@ -4,10 +4,14 @@
             [clj-http.client :as http]
             [partnorize-api.middleware.config :as config]))
 
-(defn generate-message [{:keys [api-key]} prompt]
-  (let [body {:model "gpt-3.5-turbo"
-              :messages [{:role "system"
-                          :content "You are a helpful assistant. Please ignore html in the prompts. Please generate your response in html"}
+(defn generate-message 
+  ([ai-config prompt]
+   (generate-message ai-config prompt nil))
+  ([{:keys [api-key]} prompt context]
+  (let [body {:model "gpt-4o-mini"
+              :messages [
+                         {:role "system"
+                          :content (or context "You are a helpful assistant. Please ignore html in the prompts. Please generate your response in html")}
                          {:role "user"
                           :content prompt}]}
         response (http/post "https://api.openai.com/v1/chat/completions"
@@ -16,22 +20,31 @@
                              :accept :json
                              :oauth-token api-key
                              :body (json/generate-string body)})]
-    (-> response :body :choices first :message :content)))
+    (-> response :body :choices first :message :content))))
 
 (comment
   (generate-message (:open-ai config/config) "Write 2-3 sentences introducing myself in a friendly tone to someone named Tom Singell. We are connected because we both love Pickleball")
   ;
   )
 
-(let [context {:buyer-name "Matt Nicosia"
-               :buyer-job-title "Director of Growth"
-               :buyer-account "Crossbeam"
-               :buyer-location "Pittsburgh"
-               :buyer-website "crossbeam.com"
-               :seller-name "Ryan Echternacht"
-               :seller-job-title "CTO"
-               :seller-company "Sharepage"}
-      context-prompt (slurp "resources/ai/context-prompt.mustache")
-      rendered-context (stache/render context-prompt context)]
-  (generate-message (:open-ai config/config) rendered-context))
+(comment
 
+  (let [prompt "Write top 10 quotes reflecting what my persona is most likely to think when they experience pains and frustrations when trying to get their job done.
+             
+             Now pick out the top 3 most likely given their persona. Write each with a 3 to 7 word header followed by one or two sentences that summarize. 
+                    
+                    Only provide the answer, no other context. Use html formatting, but only <ul>, <li>, <b>, and <i> tags"
+        context {:buyer-name "Chad Spain"
+                 :buyer-job-title "Director of Growth"
+                 :buyer-account "Zello"
+                 :buyer-location "Austin, Texas"
+                 :buyer-website "crossbeam.com"
+                 :seller-name "Archer"
+                 :seller-job-title "Account Executive"
+                 :seller-company "Sharepage"
+                 :seller-website "https://www.scratchpad.com"}
+        context-prompt (slurp "resources/ai/context-prompt.mustache")
+        rendered-context (stache/render context-prompt context)]
+    (generate-message (:open-ai config/config) prompt rendered-context))
+  ;
+  )
