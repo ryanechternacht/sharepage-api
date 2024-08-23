2a4cfa6be6ac4b62e36c027b169ae47180e90e5b
ryan echternacht <ryan@echternacht.org>
2024-08-23 16:59:09 -0400

    Add unsplash utm codes

--- src/partnorize_api/external_api/unsplash.clj
+++ src/partnorize_api/external_api/unsplash.clj
@@ -3,14 +3,15 @@
             [clojure.string :as str]
             [partnorize-api.middleware.config :as config]))
 
-(defn- pull-out-image-info [unsplash-result]
+(defn- reformat-for-app [unsplash-app unsplash-result]
   {:blurhash (-> unsplash-result :blur_hash)
-   :url (str (-> unsplash-result :urls :full) "&w=2400")
+   :url (str (-> unsplash-result :urls :full)
+             "&w=2400&utm_source=" unsplash-app "&utm_medium=referral")
    :author {:name (-> unsplash-result :user :name)
             :link (-> unsplash-result :user :links :self)}})
 
 (defn search-unsplash
-  [{:keys [api-key]} query]
+  [{:keys [api-key app-name]} query]
   (let [url (cond-> "https://api.unsplash.com/search/photos?orientation=landscape&per_page=16"
               (not (str/blank? query)) (str "&query=" query))]
     (->> (http/get url
@@ -20,7 +21,7 @@
                     :headers {:Authorization (str "Client-ID " api-key)}})
          :body
          :results
-         (map pull-out-image-info))))
+         (map #(reformat-for-app app-name %)))))
 
 (comment
   (search-unsplash (:unsplash config/config) "office")
