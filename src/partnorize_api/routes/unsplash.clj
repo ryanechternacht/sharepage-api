8288b6dbcb6b84acff10b40c4799befcad8462c9
ryan echternacht <ryan@echternacht.org>
2024-08-26 12:53:09 -0400

    hit unsplash download link and add author utms

--- src/partnorize_api/routes/unsplash.clj
+++ src/partnorize_api/routes/unsplash.clj
@@ -4,7 +4,7 @@
             [partnorize-api.middleware.prework :as prework]
             [ring.util.http-response :as response]))
 
-(def GET-search-unpslash
+(def GET-search-unsplash
   (cpj/GET "/v0.1/search-unsplash/:query" [query :as original-req]
     (let [{:keys [prework-errors config]}
           (prework/do-prework original-req
@@ -12,3 +12,14 @@
       (if (seq prework-errors)
         (prework/generate-error-response prework-errors)
         (response/ok (unsplash/search-unsplash (:unsplash config) query))))))
+
+
+;; The body is mising?
+(def POST-download-unsplash
+  (cpj/POST "/v0.1/download-unsplash" original-req
+    (let [{:keys [prework-errors config body]}
+          (prework/do-prework original-req
+                              (prework/ensure-is-org-member))]
+      (if (seq prework-errors)
+        (prework/generate-error-response prework-errors)
+        (response/ok (unsplash/unsplash-download (:unsplash config) (:link body)))))))
